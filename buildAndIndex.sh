e4976fbef76c5d35ce6f56b460cffb3d36e21817
Martijn Maas <martijn.maas@huygens.knaw.nl>
2020-01-27 14:28:26 +0100

    Remove test script

--- buildAndIndex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mvn clean package
-
-./target/appassembler/bin/resussun createIndex --tim https://repository.huygens.knaw.nl --dataSet u519bd710306620fa7c56d541ae7b9f5b7f57a706__clusius
