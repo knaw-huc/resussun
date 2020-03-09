b6e4196ca93ee73da65d186ae0c5b89cbce33b5a
Martijn Maas <martijn.maas@huygens.knaw.nl>
2020-03-09 09:55:45 +0100

    Start supporting applications for local build

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,5 +2,8 @@
 
 mvn clean package
 
+docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.6.1
+docker run -d -p 6379:6379 redis:5.0.7
+
 ./target/appassembler/bin/resussun server config.yml 
 
